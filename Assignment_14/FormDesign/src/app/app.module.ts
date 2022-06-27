ef71b7f338cc4a192707387e275f238198f1326e
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2022-06-27 23:57:55 +0530

    new updated angular application

---
+++ Assignment_14/FormDesign/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveComponent } from './reactive/reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ MEAN/Server/app.js
@@ -0,0 +1,30 @@
+const { response } = require('express');
+
+express = require('express');
+eobj = express();
+port = 5100;
+
+function MarvellousConnect(request, response)
+{
+    console.log("Marvellous Server is live at port 5100");
+}
+eobj.listen(port, MarvellousConnect);
+
+function MarvellousRoot(request, response)
+{
+    response.json({ "Status": "Success" });
+}
+eobj.get('/', MarvellousRoot);
+
+function MarvellousBatches(request, response)
+{
+    response.json({ "PPA": "4 months", "Python": "3 months", "Angular": "5 months" });
+}
+eobj.get('/getBatches', MarvellousBatches);
+
+function MarvellousAdmin(request, response)
+{
+    response.json({ "Mobile": "907519437", "Website": "www.marvellousinfosytems.com" });
+}
+
+eobj.get('/getAdminDetails', MarvellousAdmin);
